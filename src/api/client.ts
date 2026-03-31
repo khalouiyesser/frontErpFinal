1199d4b54008606116966efaa579d357a63927c9
khaloui yesser <khaluiyesser@gmail.com>
2026-03-31 02:04:15 +0100

    deploy

--- src/api/client.ts
+++ src/api/client.ts
@@ -42,9 +42,9 @@ import toast from 'react-hot-toast';
 
 // ✅ Après
 // const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
-const API_URL = 'http://192.168.1.176:3001';
+// const API_URL = 'http://192.168.1.176:3001';
 // const API_URL = 'http://localhost:3001';
-// const API_URL = 'https://kypro-backend.onrender.com';
+const API_URL = 'https://kypro-backend.onrender.com';
 
 
 const api = axios.create({
